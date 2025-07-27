ae5bf39d17a368f012bd657902e09927b633447c
Justin Angelo V. VIllacorta <127913981+JustinVillacorta@users.noreply.github.com>
2025-07-27 22:28:19 +0800

    screens and bottomNavbar added(to be fixed)

---
+++ lib/features/auth/presentation/pages/Payroll/payroll_views/payroll_Views.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class payrollScreen extends StatelessWidget {
+  const payrollScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
