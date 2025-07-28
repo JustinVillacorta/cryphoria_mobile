be8ea62cfb8d8a42b1b2fe0b51b63175baf8c5c4
Justin Angelo V. VIllacorta <127913981+JustinVillacorta@users.noreply.github.com>
2025-07-28 16:04:44 +0800

    Switch to Cupertino UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:cryphoria_mobile/features/presentation/widgets/widgetTree.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: WidgetTree(),
-    debugShowCheckedModeBanner: false,
-  ));
+  runApp(
+    const CupertinoApp(
+      home: WidgetTree(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
 }
