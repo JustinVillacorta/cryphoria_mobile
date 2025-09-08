5e64f5e22674b7cea14b63fa82cd47edba24ff6e
Justin Angelo V. VIllacorta <127913981+JustinVillacorta@users.noreply.github.com>
2025-09-08 23:52:43 +0800

    authentication update

---
+++ lib/features/domain/usecases/usecases.dart
@@ -0,0 +1,18 @@
+// Authentication Use Cases
+export 'Login/login_usecase.dart';
+export 'Register/register_use_case.dart';
+
+// Logout Use Cases
+export 'Logout/logout_usecase.dart';
+export 'Logout/logout_check_usecase.dart';
+export 'Logout/logout_force_usecase.dart';
+
+// Session Management Use Cases
+export 'Session/approve_session_usecase.dart';
+export 'Session/confirm_password_usecase.dart';
+export 'Session/get_sessions_usecase.dart';
+export 'Session/get_transferable_sessions_usecase.dart';
+export 'Session/revoke_other_sessions_usecase.dart';
+export 'Session/revoke_session_usecase.dart';
+export 'Session/transfer_main_device_usecase.dart';
+export 'Session/validate_session_usecase.dart';
